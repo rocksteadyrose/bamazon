854fbc8471083a53404d82fe20c3b4fecdf10e38
Leslie Rose <leslierose@Leslies-MBP.attlocal.net>
2018-05-15 00:42:21 -0500

    completed functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require('cli-table');
+var chosenItem;
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -21,30 +22,32 @@ connection.connect(function (err) {
 
 
 function showList() {
-    connection.query("SELECT * FROM products", function(err, response) {
+    connection.query("SELECT * FROM products", function (err, response) {
 
-    var table = new Table({
-        head: ['ID', 'Product', 'Price'],
-        widths: [80, 80, 80]
-    });
+        var table = new Table({
+            head: ['ID', 'Product', 'Price'],
+            colWidths: [5, 50, 10]
+        });
 
-    for (var i = 0; i < response.length; i++) {
-        table.push([response[i].item_id + response[i].product_name + response[i].price])
-    }
-    console.log(table.toString()); 
+        for (var i = 0; i < response.length; i++) {
+            table.push([response[i].item_id, response[i].product_name, response[i].price])
+        }
+        console.log(table.toString());
 
-    updateItems ()
+        updateItems();
 
-})}
+    })
+}
 
-    function updateItems () {
+function updateItems() {
 
     connection.query("SELECT * FROM products", function (err, response) {
 
         if (err) throw err;
         inquirer
             .prompt([
-                {   name: "choice",
+                {
+                    name: "choice",
                     type: "input",
                     message: "What is the ID of the item you'd like to purchase?"
                 },
@@ -55,19 +58,16 @@ function showList() {
                 }
             ])
             .then(function (answer) {
-                var chosenItem;
-                connection.query("SELECT * FROM products WHERE item_id=?", answer.choice, function(err, response) 
-                {
+                connection.query("SELECT * FROM products WHERE item_id=?", answer.choice, function (err, response) {
                     for (var i = 0; i < response.length; i++) {
                         if (response[i].item_id == answer.choice) {
-                        chosenItem = response[i];
-                        console.log(chosenItem)
+                            chosenItem = response[i];
+                        }
                     }
-                    } 
 
                     if (answer.stock < chosenItem.stock_quantity) {
                         var stockUpdate = chosenItem.stock_quantity - answer.stock;
-                        console.log(stockUpdate)
+                        var totalCost = answer.stock * chosenItem.price;
                         var query = connection.query(
                             "UPDATE products SET ? WHERE ?",
                             [
@@ -80,17 +80,69 @@ function showList() {
                             ],
                             function (error) {
                                 if (error) throw err;
-                                console.log("Purchased successfully!");
-                                //   start();
+                                console.log("Purchased successfully! Your order total is " + "$" + totalCost);
+                                successfulPurchase();
                             }
                         );
+
                     }
-                    // else {
-                    //     console.log("Insufficient quantity!");
-                    //     console.log(chosenItem)
-                    //     //   start();
-                    // }
+                    else {
+                        console.log("Insufficient quantity!");
+                        unsuccessfulPurchase();
+                    }
                 }
-            )
-            })})
+
+                )
+
+            })
+
+    })
+}
+
+function successfulPurchase() {
+    inquirer.prompt([
+        {
+            name: "confirm",
+            type: "confirm",
+            message: "Would you like to make another purchase?"
         }
+    ])
+        .then(function (answer) {
+            switch (answer.confirm) {
+            case true:
+                showList();
+                break;
+            case false:
+                console.log("See you later!")
+                process.exit();
+                break;
+            }
+        })
+}
+
+function unsuccessfulPurchase() {
+    inquirer.prompt([
+        {
+            name: "list",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "Purchase a different item or less of this item",
+                "Come back later"
+              ]
+        }
+    ])
+        .then(function (answer) {
+            switch (answer.list) {
+                case "Purchase a different item or less of this item":
+                showList();
+                  break;
+          
+                case "Come back later":
+                    console.log("See you later!")
+                    process.exit();
+                  break;
+                }
+        })
+
+}
