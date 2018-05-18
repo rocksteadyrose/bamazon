0bcdb475fe3b3ba87edde6bb40320a41b9f03c7b
Leslie Rose <leslierose@Leslies-MBP.attlocal.net>
2018-05-18 12:36:36 -0500

    updates to customer functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,8 +4,6 @@ var Table = require('cli-table');
 var chalk = require('chalk');
 var figlet = require('figlet');
 
-var bypass = true;
-
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -31,9 +29,8 @@ connection.connect(function (err) {
     showTable();
 });
 
-function showTable() {
+function showTable(newPurchases) {
     connection.query("SELECT * FROM products", function (err, response) {
-
         var table = new Table({
             head: ['ID', 'Product', 'Price', 'Product Sales'],
             colWidths: [5, 50, 10, 20]
@@ -43,143 +40,159 @@ function showTable() {
             table.push([response[i].item_id, response[i].product_name, response[i].price, response[i].product_sales])
         }
         console.log(table.toString());
-    if (bypass === true) {
-        updateItems();
-    }
 
+        if (newPurchases) {
+            inquirer.prompt({
+                name: "updateTable",
+                type: "rawlist",
+                message: "What would you like to do now?",
+                choices: ["Purchase another item",
+                    "Come back later"]
+            })
+                .then(function (answer) {
+                    switch (answer.updateTable) {
+                        case "Purchase another item":
+                            updateItems()
+                            break;
+                        case "Come back later":
+                            leave();
+                            break;
+                    }
+                });
+        } else {updateItems();
+        }
     })
 }
 
 function updateItems() {
-    connection.query("SELECT * FROM products", function (err, response) { 
-    inquirer
-        .prompt([
-            {
-                name: "choice",
-                type: "input",
-                message: "What is the ID of the item you'd like to purchase?",
-                validate: function(input) {
-                    var IDarray = []
-                    for (var i = 0; i < response.length; i++) {
-                        var IDs = response[i].item_id;
-                        IDarray.push(IDs)
-                    }
-                    if (IDarray.indexOf(Number(input)) > -1) {
-                      return true;
-                    }
-                    console.log(chalk.red("\n" + "Item doesn't exist! Please pick another!"))
-                    return false;
-                }
-            },
-            {
-                name: "stock",
-                type: "input",
-                message: "How many units would you like to purchase?",
-                validate: function(value) {
-                    if (isNaN(value) === false && value > 0) {
-                      return true;
-                    }
-                    else {
-                    console.log(chalk.red("\n" + "Please enter a valid number."))
-                    return false;
-                    }
-            }
-        }
-        ])
-        .then(function (answer) {
-            var chosenItem;
-            connection.query("SELECT * FROM products WHERE item_id=?", answer.choice, function (err, response) {
-                for (var i = 0; i < response.length; i++) {
-                    if (Number(response[i].item_id) === Number(answer.choice)) {
-                        chosenItem = response[i];
-                    }
-                }
-
-                if (answer.stock <= chosenItem.stock_quantity) {
-                    var stockUpdate = chosenItem.stock_quantity - answer.stock;
-                    var totalCost = answer.stock * chosenItem.price;
-                    var itemName = chosenItem.product_name;
-
-                    var query = connection.query(
-                        "UPDATE products SET ? WHERE ?",
-                        [
-                            {
-                                stock_quantity: stockUpdate,
-                                product_sales: totalCost
-
-                            },
-                            {
-                                item_id: answer.choice
-                            }
-                        ],
-                        
-                        function (error) {
-                            if (error) throw err;
-                            console.log(chalk.green("Purchased successfully! Your order total for " + itemName + " is " + "$" + totalCost));
-                            bypass = false;
-                            successfulPurchase();
+    connection.query("SELECT * FROM products", function (err, response) {
+        inquirer
+            .prompt([
+                {
+                    name: "choice",
+                    type: "input",
+                    message: "What is the ID of the item you'd like to purchase?",
+                    validate: function (input) {
+                        var IDarray = []
+                        for (var i = 0; i < response.length; i++) {
+                            var IDs = response[i].item_id;
+                            IDarray.push(IDs)
                         }
-                    );
+                        if (IDarray.indexOf(Number(input)) > -1) {
+                            return true;
+                        }
+                        console.log(chalk.red("\n" + "Item doesn't exist! Please pick another!"))
+                        return false;
+                    }
+                },
+                {
+                    name: "stock",
+                    type: "input",
+                    message: "How many units would you like to purchase?",
+                    validate: function (value) {
+                        if (isNaN(value) === false && value > 0) {
+                            return true;
+                        }
+                        else {
+                            console.log(chalk.red("\n" + "Please enter a valid number."))
+                            return false;
+                        }
+                    }
                 }
-                else {
-                    console.log(chalk.red("Insufficient quantity!"));
-                    unsuccessfulPurchase();
-                }
-            })
-        })
-        }
-
-        )
-}
-
-function successfulPurchase() {
-    
-   showTable();
-    
-        inquirer.prompt([
-            {
-                name: "confirm",
-                type: "confirm",
-                message: "Would you like to make another purchase?"
-            }
-        ])
+            ])
             .then(function (answer) {
-                switch (answer.confirm) {
-                    case true:
-                        bypass = true;
-                        showTable();
-                        break;
-                    case false:
-                        console.log(chalk.blue("See you later!"))
-                        process.exit();
-                        break;
-                }
+                var chosenItem;
+                connection.query("SELECT * FROM products WHERE item_id=?", answer.choice, function (err, response) {
+                    for (var i = 0; i < response.length; i++) {
+                        if (Number(response[i].item_id) === Number(answer.choice)) {
+                            chosenItem = response[i];
+                        }
+                    }
+
+                    var inStock = (answer.stock <= chosenItem.stock_quantity);
+                    if (!inStock) {
+                        console.log(chalk.red("Insufficient quantity!"));
+                        handlePurchase(inStock);
+                    } else {
+
+                        var stockUpdate = chosenItem.stock_quantity - answer.stock;
+                        var productSales = chosenItem.product_sales;
+                        var purchasePrice = answer.stock * chosenItem.price;
+                        var total = (answer.stock * chosenItem.price) + productSales;
+                        var itemName = chosenItem.product_name;
+
+                        var query = connection.query(
+                            "UPDATE products SET ? WHERE ?",
+                            [
+                                {
+                                    stock_quantity: stockUpdate,
+                                    product_sales: total
+                                },
+                                {
+                                    item_id: answer.choice
+                                }
+                            ],
+
+                            function (error) {
+                                if (error) throw err;
+                                console.log(chalk.green("\n" + "Purchased successfully! Your order total for " + itemName + " is " + "$" + purchasePrice));
+                                handlePurchase(inStock);
+                            });
+                    }
+                })
             })
+    })
 }
 
-function unsuccessfulPurchase() {
+function handlePurchase(result) {
+
+    //Variables from the ternary operator below
+
+    var success = {
+        name: "list",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View updated store", 
+            "Make another purchase",
+            "Come back later"
+        ]
+    }
+
+    var failure = {
+        name: "list",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "Purchase a different item or less of this item", "Come back later"
+        ]
+    }
+
+    //Result will be a success if the result of the purchase from the Update Items function was true, and a failure if the purchase was false.
+    var response = result ? success : failure;
+
+    //Then we assign our success/failure variables at the top and call them into the response depending on which was chosen
     inquirer.prompt([
-        {
-            name: "list",
-            type: "list",
-            message: "What would you like to do?",
-            choices: [
-                "Purchase a different item or less of this item",
-                "Come back later"
-            ]
-        }
+        response
     ])
         .then(function (answer) {
             switch (answer.list) {
+                case "View updated store":
+                    var updateTable = true;
+                    showTable(updateTable);
+                    break;
                 case "Purchase a different item or less of this item":
+                case "Make another purchase":
                     showTable();
                     break;
-
                 case "Come back later":
-                    console.log(chalk.blue("See you later!"))
-                    process.exit();
+                    leave();
                     break;
             }
         })
+}
 
+function leave() {
+    console.log(chalk.blue("See you later!"))
+    process.exit();
 }
